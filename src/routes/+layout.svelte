03181f7ffaae741fed690b7e2d5ca9034f1145ce
Omar Alhamad <omar93@hotmail.se>
2024-02-11 13:45:29 +0100

    reset css
    Added a reset.css, reset ALL css styles, it is used in +layout.svelte
    Start working on the layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import '$lib/reset.css'
+</script>
+
+<div id="page--wrapper">
+  <header>Header</header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>Footer</footer>
+</div>
+
+<style>
+  #page--wrapper {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
